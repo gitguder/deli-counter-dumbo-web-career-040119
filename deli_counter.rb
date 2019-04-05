be4ab6993aa824a58d1516d2fa237849ed6326f8
Codey Pinero <codey.pinero@gmail.com>
2019-04-05 01:21:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,9 +17,5 @@ def take_a_number(katz_deli, person)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.size == 0
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{katz_deli.shift}."
-  end
+  katz_deli.size == 0 ? puts "There is nobody waiting to be served!" : puts "Currently serving #{katz_deli.shift}."
 end
